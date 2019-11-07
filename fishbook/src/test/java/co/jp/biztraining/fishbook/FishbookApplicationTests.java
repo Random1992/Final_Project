8e2ce9a2e1e4fc4502931234e7df53d069cc1b57
Taimin Zhang <taimin.zhang@bizreach.co.jp>
2019-11-07 17:45:37 +0900

    Fishbook initial

---
+++ fishbook/src/test/java/co/jp/biztraining/fishbook/FishbookApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jp.biztraining.fishbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
